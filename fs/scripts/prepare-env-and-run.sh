75bc34680d36aa2ff1762ea20b7db92691ddccc9
Catalin Manolescu <cc.manolescu@gmail.com>
2020-12-15 13:29:11 +0200

    enable logs

--- fs/scripts/prepare-env-and-run.sh
+++ fs/scripts/prepare-env-and-run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
+_log() {
+  echo "`date +"%Y-%m-%d %T %Z"`   ${1}"
+}
 
 export PG_SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH=/usr/lib/postgresql/${PG_VERSION}/bin:$PATH
@@ -14,17 +19,24 @@ PG_SQL_SCRIPTS_PATH="/etc/postgresql/scripts"
 rm -rf $PG_DATA_PATH/container_ready
 rm -rf $PG_DATA_PATH/postmaster.pid
 
-
 if [ -n "$PG_PORT" ]; then
-  echo "== set postgresql port to '$PG_PORT'"
+  _log "== set postgresql port to '$PG_PORT'"
   sed -i "s/^port\\ =.*/port\\ =\\ $PG_PORT/g" $PG_CONFIG
 fi
 
-echo "== set default postgresql configuration"
+_log "== set default postgresql configuration"
 # echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
 sed -i "s|^local\\ *all\\ *all.*|local\\ all\\ all\\ trust|g" /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
 sed -i "s|^[#]*listen_addresses\\ =.*|listen_addresses\\ =\\ '*'|g" $PG_CONFIG
 sed -i "s|^[#]*data_directory\\ =.*|data_directory\\ =\\ '${PG_DATA_PATH}'|g" $PG_CONFIG
+
+sed -i "s|^[#]*log_min_duration_statement\\ =.*|log_min_duration_statement\\ =\\ 500|g" $PG_CONFIG
+sed -i "s|^[#]*log_checkpoints\\ =.*|log_checkpoints\\ =\\ on|g" $PG_CONFIG
+sed -i "s|^[#]*log_connections\\ =.*|log_connections\\ =\\ on|g" $PG_CONFIG
+sed -i "s|^[#]*log_disconnections\\ =.*|log_disconnections\\ =\\ on|g" $PG_CONFIG
+sed -i "s|^[#]*log_duration\\ =.*|log_duration\\ =\\ off|g" $PG_CONFIG
+sed -i "s|^[#]*log_lock_waits\\ =.*|log_lock_waits\\ =\\ on|g" $PG_CONFIG
+sed -i "s|^[#]*log_statement\\ =.*|log_statement\\ =\\ none|g" $PG_CONFIG
 
 PG_SHARED_LIBS='pg_stat_statements,pg_repack'
 sed -i "s|^[#]*shared_preload_libraries\\ =.*|shared_preload_libraries\\ =\\ '${PG_SHARED_LIBS}'|g" $PG_CONFIG
@@ -36,16 +48,16 @@ if [ -z "$POSTGRES_ENCODING" ]; then
 fi
 
 if [ ! "`ls -A ${PG_DATA_PATH}`" ]; then
-  echo "== initialise postgres"
+  _log "== initialise postgres"
   initdb -E $POSTGRES_ENCODING -D $PG_DATA_PATH
   export PG_FIRST_START=1
 fi
 
-echo "== starting postgresql server"
+_log "== starting postgresql server"
 /usr/lib/postgresql/${PG_VERSION}/bin/postgres -D /var/lib/postgresql/${PG_VERSION}/main -c config_file=${PG_CONFIG} &
 
 while ! pg_isready  > /dev/null 2> /dev/null; do
-  echo ">> waiting for postgresql server to start"
+  _log ">> waiting for postgresql server to start"
   sleep 1
 done
 
@@ -62,6 +74,6 @@ fi
 . ${PG_SCRIPTS_PATH}/run-sql-commands.sh
 
 touch $PG_DATA_PATH/container_ready
-echo "== container ready"
+_log "== container ready"
 
 while true; do sleep 10;done
