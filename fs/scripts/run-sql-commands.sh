d1d773b52e9475a91a7600d585f5198826d09265
Catalin Manolescu <cc.manolescu@gmail.com>
2021-06-27 16:43:59 +0300

    rename run scripts env var

--- fs/scripts/run-sql-commands.sh
+++ fs/scripts/run-sql-commands.sh
@@ -1,12 +1,12 @@
-if [ -n "$RUN_SQL" ]; then
+if [ -n "$POSTGRES_RUN_SQL" ]; then
   ORIGIN_IFS=$IFS
   IFS=$'\n'
-  for db_name in `echo -n $RUN_SQL | jq -r 'keys'[]`;do
-    for sql_cmd in `echo -n $RUN_SQL | jq -r .${db_name}[]`; do
+  for db_name in `echo -n $POSTGRES_RUN_SQL | jq -r 'keys'[]`;do
+    for sql_cmd in `echo -n $POSTGRES_RUN_SQL | jq -r .${db_name}[]`; do
         echo "== running sql: ${db_name}:${sql_cmd}"
         if [ "$db_name" = '_' ]; then
           psql --command "${sql_cmd};"
-        else 
+        else
           psql -d $db_name --command "${sql_cmd};"
         fi
     done
